5af7f2cca1a1fd94cb8a8373419a07ab6339a6ff
Никита Кобытев <nikitakobytev@mail.ru>
2026-05-12 23:44:26 +0700

    fix gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,17 +19,23 @@ repositories {
 }
 
 dependencies {
+
+    // Javalin
     implementation("io.javalin:javalin:6.6.0")
     implementation("io.javalin:javalin-rendering:6.6.0")
-    implementation("io.javalin:javalin-bundle:6.6.0")
+
+    // Логи
     implementation("org.slf4j:slf4j-simple:2.0.7")
 
+    // База данных
     implementation("com.zaxxer:HikariCP:5.1.0")
-    implementation("gg.jte:jte:3.1.9")
     implementation("com.h2database:h2:2.3.232")
     implementation("org.postgresql:postgresql:42.7.5")
+
+    // JTE
     implementation("gg.jte:jte:3.1.9")
 
+    // Тесты
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
 }
@@ -39,6 +45,7 @@ tasks.test {
 
     testLogging {
         exceptionFormat = TestExceptionFormat.FULL
+
         events = mutableSetOf(
             TestLogEvent.FAILED,
             TestLogEvent.PASSED,

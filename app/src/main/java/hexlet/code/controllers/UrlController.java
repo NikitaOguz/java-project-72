428bddc95a07fc4d2cc11881d50c23aa9fa11181
Никита Кобытев <nikitakobytev@mail.ru>
2026-05-16 02:51:55 +0700

    fix error

--- app/src/main/java/hexlet/code/controllers/UrlController.java
+++ app/src/main/java/hexlet/code/controllers/UrlController.java
@@ -31,8 +31,11 @@ public class UrlController {
             Url existing = UrlRepository.findByName(normalized);
 
             if (existing != null) {
-                ctx.sessionAttribute("flash", "Страница уже существует");
-                ctx.redirect("/urls/" + existing.getId());
+                var page = new HashMap<String, Object>();
+                page.put("url", existing);
+                page.put("checks", UrlCheckRepository.findByUrlId(existing.getId()));
+                page.put("flash", "Страница уже существует");
+                ctx.render("urls/show.jte", page);
                 return;
             }
 
@@ -47,7 +50,6 @@ public class UrlController {
 
             var page = new HashMap<String, Object>();
             page.put("flash", "Некорректный URL");
-
             ctx.render("index.jte", page);
         }
     }
